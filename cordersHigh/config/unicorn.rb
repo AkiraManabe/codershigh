b2dc8b41a095ab67d0ff208c66b1df3689179942
irie <girlscoutsjp@irie.com>
2015-12-23 03:41:03 +0900

    add unicorn

---
+++ cordersHigh/config/unicorn.rb
@@ -0,0 +1,11 @@
+worker_processes 2
+
+listen 8080, :tcp_nopush => true
+
+listen '/tmp/unicorn.sock'
+pid '/tmp/unicorn.pid'
+
+stderr_path File.expand_path('log/unicorn.log')
+stdout_path File.expand_path('log/unicorn.log')
+
+preload_app true
